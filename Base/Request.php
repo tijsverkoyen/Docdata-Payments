989a05d129960487d4afdbb2cb9d820fcc980ef4
Tijs Verkoyen <tijs@verkoyen.eu>
2013-03-09 18:52:39 +0100

    Removed duplicate code in Request-classes

---
+++ Base/Request.php
@@ -0,0 +1,61 @@
+<?php
+namespace TijsVerkoyen\DocDataPayments\Base;
+
+/**
+ * DocDataPayments Request class
+ *
+ * @author Tijs Verkoyen <php-docdatapayments@verkoyen.eu>
+ */
+class Request extends Object
+{
+    /**
+     * Merchant credentials.
+     *
+     * @var \TijsVerkoyen\DocDataPayments\Types\Merchant
+     */
+    protected $merchant;
+
+    /**
+     * Payment order key belonging to the order which needs to be canceled.
+     *
+     * @var string
+     */
+    protected $paymentOrderKey;
+
+    /**
+     * @var string
+     */
+    protected $version = '1.0';
+
+    /**
+     * @param \TijsVerkoyen\DocDataPayments\Types\Merchant $merchant
+     */
+    public function setMerchant($merchant)
+    {
+        $this->merchant = $merchant;
+    }
+
+    /**
+     * @return \TijsVerkoyen\DocDataPayments\Types\Merchant
+     */
+    public function getMerchant()
+    {
+        return $this->merchant;
+    }
+
+    /**
+     * @param string $paymentOrderKey
+     */
+    public function setPaymentOrderKey($paymentOrderKey)
+    {
+        $this->paymentOrderKey = $paymentOrderKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentOrderKey()
+    {
+        return $this->paymentOrderKey;
+    }
+}
--- Types/StatusRequest.php
+++ Types/StatusRequest.php
@@ -1,63 +1,13 @@
 <?php
 namespace TijsVerkoyen\DocDataPayments\Types;
 
-use TijsVerkoyen\DocDataPayments\Base\Object;
+use TijsVerkoyen\DocDataPayments\Base\Request;
 
 /**
  * DocDataPayments StatusRequest class
  *
  * @author Tijs Verkoyen <php-docdatapayments@verkoyen.eu>
  */
-class StatusRequest extends Object
+class StatusRequest extends Request
 {
-    /**
-     * Merchant credentials.
-     *
-     * @var \TijsVerkoyen\DocDataPayments\Types\Merchant
-     */
-    protected $merchant;
-
-    /**
-     * Payment order key belonging to the order which needs to be canceled.
-     *
-     * @var string
-     */
-    protected $paymentOrderKey;
-
-    /**
-     * @var string
-     */
-    protected $version = '1.0';
-
-    /**
-     * @param \TijsVerkoyen\DocDataPayments\Types\Merchant $merchant
-     */
-    public function setMerchant($merchant)
-    {
-        $this->merchant = $merchant;
-    }
-
-    /**
-     * @return \TijsVerkoyen\DocDataPayments\Types\Merchant
-     */
-    public function getMerchant()
-    {
-        return $this->merchant;
-    }
-
-    /**
-     * @param string $paymentOrderKey
-     */
-    public function setPaymentOrderKey($paymentOrderKey)
-    {
-        $this->paymentOrderKey = $paymentOrderKey;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPaymentOrderKey()
-    {
-        return $this->paymentOrderKey;
-    }
 }
