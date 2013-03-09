755dbfb96f34835c8a15be04d435ab9f9f2a0248
Tijs Verkoyen <tijs@verkoyen.eu>
2013-03-09 18:29:05 +0100

    Removed duplicate code for PaymentInfo-classes

---
+++ Base/PaymentInfo.php
@@ -0,0 +1,75 @@
+<?php
+namespace TijsVerkoyen\DocDataPayments\Base;
+
+use TijsVerkoyen\DocDataPayments\Base\Object;
+
+/**
+ * DocDataPayments PaymentInfo class
+ *
+ * @author Tijs Verkoyen <php-docdatapayments@verkoyen.eu>
+ */
+class PaymentInfo extends Object
+{
+    /**
+     * @var string
+     */
+    protected $cardHolder;
+
+    /**
+     * @var string
+     */
+    protected $cardCountry;
+
+    /**
+     * @var string
+     */
+    protected $panLast4;
+
+    /**
+     * @param string $cardCountry
+     */
+    public function setCardCountry($cardCountry)
+    {
+        $this->cardCountry = $cardCountry;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardCountry()
+    {
+        return $this->cardCountry;
+    }
+
+    /**
+     * @param string $cardHolder
+     */
+    public function setCardHolder($cardHolder)
+    {
+        $this->cardHolder = $cardHolder;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardHolder()
+    {
+        return $this->cardHolder;
+    }
+
+    /**
+     * @param string $panLast4
+     */
+    public function setPanLast4($panLast4)
+    {
+        $this->panLast4 = $panLast4;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPanLast4()
+    {
+        return $this->panLast4;
+    }
+}
