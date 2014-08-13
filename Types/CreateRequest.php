225122bad091ea250578630b6a0b8d485018bb85
Theo den Hollander <theo@hollandware.com>
2014-08-13 17:30:14 +0200

    Api 1.2

--- Types/CreateRequest.php
+++ Types/CreateRequest.php
@@ -102,7 +102,7 @@ class CreateRequest extends Object
     /**
      * @var string
      */
-    protected $version = '1.1';
+    protected $version = '1.2';
 
     /**
      * @param \TijsVerkoyen\DocDataPayments\Types\Destination $billTo
